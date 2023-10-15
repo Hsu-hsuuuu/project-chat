cd69aab0dd3ce20b3e8d89fcb5d17d6eed6e6546
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-10-16 01:33:13 +0300

    created a folder "ChatHeader" and a  component

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,10 +1,12 @@
 import React, { FC, useState } from 'react';
-import styles from './Chat.module.css';
+import styles from './Chat.module.scss';
 
 const Chat: FC = () => {
   return (
     <div className={ styles.chat }>
-      
+      <div className={ styles.chat_window }>
+        CHAT WINDOW
+      </div>
     </div>
   );
 }
---
+++ src/components/ChatHeader/ChatHeader.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const ChatHeader: FC = () => {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+  
+  export default ChatHeader;
