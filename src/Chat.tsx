d07071d1180fc89cc7c5974c6c9bc5638fc680df
Andrey Natarov <andreynatarov@MacBook-Air-Andrey.local>
2023-10-16 00:57:13 +0300

    added main background and size

---
+++ src/Chat.tsx
@@ -0,0 +1,12 @@
+import React, { FC, useState } from 'react';
+import styles from './Chat.module.css';
+
+const Chat: FC = () => {
+  return (
+    <div className={ styles.app }>
+      
+    </div>
+  );
+}
+
+export default Chat;
