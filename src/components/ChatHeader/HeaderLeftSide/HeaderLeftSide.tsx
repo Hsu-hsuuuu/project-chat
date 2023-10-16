f4ae17cf03cf7965d75b6c0d4741edd90fe916fd
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-10-17 01:07:49 +0300

    added styles for HeaderLeftSide

--- src/components/ChatHeader/HeaderLeftSide/HeaderLeftSide.tsx
+++ src/components/ChatHeader/HeaderLeftSide/HeaderLeftSide.tsx
@@ -5,11 +5,12 @@ const HeaderLeftSide: FC = () => {
     return (
         <div className={ styles.left_side }>
             <h1>CHAT</h1>
-            <div>
-                <h3>Profile</h3>
-                <img src="" alt="logo_image" />
+            <div className={ styles.my_profile_block }>
+                <h3 className={ styles.profile_text }>Profile</h3>
+                <button className={ styles.my_profile_button }>
+                    
+                </button> {/*&& <MyProfile /> */}
             </div>
-            <button></button> {/*&& <MyProfile /> */}
         </div>
     );
 }
