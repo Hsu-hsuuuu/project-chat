11ed9948670bf2e5382ba817811094c52f4a7fe7
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-10-19 23:33:43 +0300

    test

--- src/components/MainArea/MainLeftSide/ContactCard/ContactCrad.tsx
+++ src/components/MainArea/MainLeftSide/ContactCard/ContactCrad.tsx
@@ -1,7 +1,42 @@
 import React, { FC } from 'react';
+import fetch from 'node-fetch';
 import styles from './ContactCard.module.scss'
 
+
+
+interface Contact {
+    id: number;
+    photo: string;
+    name: string;
+    lastname: string;
+    lastmsg: string;
+    countmsg?: number;
+}
+
+
 const ContactCard: FC = () => {
+
+    const fetchContact = async (): Promise<Contact []> =>
+        fetch('https://mocki.io/v1/084c9795-e9b3-4fa0-8a7b-dfed02fdbf29')
+            .then((response) => {
+                // if (!response.ok) {
+                //     throw new Error(response.statusText)
+                // }
+                return response.json()
+            })
+            .then((data) => {
+                console.log(data)
+                return data as Contact[]
+            })
+            .catch((error) => {
+                console.error(error)
+                return []
+            })
+
+    fetchContact()
+
+    console.log()
+
     return (
         <div className={ styles.contact_card }>
             <div className={ styles.contact_photo }></div> {/* photo */}
